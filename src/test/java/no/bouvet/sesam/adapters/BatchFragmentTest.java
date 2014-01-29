e76b004eca9132747a7cbd5f2c90e080b802831c
Alf Lervag <alf.lervag@bouvet.no>
2014-01-29 13:08:49 +0100

    Added test of a batch with two fragments

--- src/test/java/no/bouvet/sesam/adapters/BatchFragmentTest.java
+++ src/test/java/no/bouvet/sesam/adapters/BatchFragmentTest.java
@@ -15,16 +15,41 @@ public class BatchFragmentTest {
 
         List<String> resources = new ArrayList<String>();
         resources.add(resourceId);
-        BatchFragment fragments = new BatchFragment(resources, source);
+        BatchFragment batch = new BatchFragment(resources, source);
 
-        List<Fragment> result = fragments.getFragments();
-        assertEquals(1, result.size());
-        Fragment fragment = result.get(0);
+        List<Fragment> fragments = batch.getFragments();
+        assertEquals(1, fragments.size());
+        Fragment fragment = fragments.get(0);
         
         assertEquals(resourceId, fragment.getResourceId());
         assertEquals(type, fragment.getType());
         assertEquals(source, fragment.getSource());
         assertEquals(3, fragment.getStatements().size());
+    }
 
+    @Test
+    public void testThatWeCanParseBatchWithTwoFragments() throws Exception {
+        String source1 = Utils.getResourceAsString("simplecase.nt");
+        String source2 = Utils.getResourceAsString("simplejournalpost.nt");
+        String resourceId1 = Utils.getFirstSubject(source1);
+        String resourceId2 = Utils.getFirstSubject(source2);
+        String source = source1 + "\n" + source2;
+
+        List<String> resources = new ArrayList<String>();
+        resources.add(resourceId1);
+        resources.add(resourceId2);
+
+        BatchFragment batch = new BatchFragment(resources, source);
+        List<Fragment> fragments = batch.getFragments();
+
+        assertEquals(2, fragments.size());
+        
+        Fragment fragment1 = fragments.get(1);
+        Fragment fragment2 = fragments.get(0);
+
+        assertEquals(resourceId1, fragment1.getResourceId());
+        assertEquals(source1, fragment1.getSource());
+        assertEquals(resourceId2, fragment2.getResourceId());
+        assertEquals(source2, fragment2.getSource());
     }
 }
