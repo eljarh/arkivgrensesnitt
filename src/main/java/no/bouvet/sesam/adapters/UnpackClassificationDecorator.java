77a5e87c044fc9c91026cff9d4b2036111e76ac3
Lars Marius Garshol <larsga@garshol.priv.no>
2014-02-19 14:23:34 +0100

    Adding some logging so I can see what's going on.

--- src/main/java/no/bouvet/sesam/adapters/UnpackClassificationDecorator.java
+++ src/main/java/no/bouvet/sesam/adapters/UnpackClassificationDecorator.java
@@ -1,5 +1,8 @@
 package no.bouvet.sesam.adapters;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import no.gecko.ephorte.services.objectmodel.v3.en.DataObjectT;
 import no.gecko.ephorte.services.objectmodel.v3.en.dataobjects.CaseT;
 import no.gecko.ephorte.services.objectmodel.v3.en.dataobjects.ClassificationT;
@@ -7,12 +10,18 @@ import no.gecko.ephorte.services.objectmodel.v3.en.dataobjects.ClassificationT;
 // FIXME: should be rewritten so that it's not specific to ClassificationT
 
 public class UnpackClassificationDecorator implements Decorator {
+    private static Logger log = LoggerFactory.getLogger(UnpackClassificationDecorator.class.getName());
+
     @Override
     public Object process(DataObjectT obj, EphorteFacade facade, BatchFragment fragment, Statement s) {
-        if (((CaseT) obj).getPrimaryClassification() != null)
+        // ePhorte complains if we try to add another ClassificationT to a
+        // case that already has one. we therefore pass if there is a value
+        ClassificationT ct = ((CaseT) obj).getPrimaryClassification();
+        log.debug("CaseT.primaryClassification {}", ct);
+        if (ct != null)
             return null; // FIXME: we should really update it
       
-        ClassificationT ct = new ClassificationT();
+        ct = new ClassificationT();
 
         String[] parts = s.object.split("::");
         for (String part : parts) {
